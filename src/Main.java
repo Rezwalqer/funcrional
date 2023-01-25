74fde4f157de5f3629b95bb3b67ecdc5f86e7d58
Ruslan <rezwalqer@gmail.com>
2023-01-25 16:05:06 +0500

    Задача по функциональному программированию

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,23 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
- 
+        String ourString1 = "The Quick Brown fox jumps over the lazy dog";
+        String ourString = ourString1.toLowerCase();
+        String[] arr = ourString.split(" ");
+        System.out.println("В тексте " + arr.length + " слов.");
+        System.out.println("TOP 10:");
+        Arrays.stream(arr)
+                .collect(Collectors.toMap(o -> o, o1 -> 1, Integer::sum)).entrySet().stream()
+                .sorted((o1, o2) -> {
+                    int equal = (o2.getValue().compareTo(o1.getValue()));
+                    if (equal == 0) {
+                        equal = o1.getKey().compareTo(o2.getKey());
+                    }
+                    return equal;
+                })
+                .limit(10)
+                .forEach(e -> System.out.println(e.getValue() + " - " + e.getKey()));
+
+
     }
 }
